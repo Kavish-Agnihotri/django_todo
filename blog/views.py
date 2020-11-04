8a39b768f9be6e6144a8fb07b06849792eff90e2
Kavish Agnihotri <kavish.agnihotri@gmail.com>
2020-11-04 20:33:37 +0530

    Added Post model and removed mock data

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Kavish Agnihotri',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': 'August 27, 2018'
-    },
-     {
-        'author': 'Rahul Vyas',
-        'title': 'Blog Post 2',
-        'content': 'Second Post Content',
-        'date_posted': 'August 28, 2018'
-    },
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'Home'
     }
     return render(request,'blog/home.html',context)
